8d8c638b16a8ce6b1c9b24190890b01057daf426
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-09-28 14:30:53 +0300

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
    
   resources :users, only: [:show] do
-    resources :items, only: [:show, :index]
+    resources :items, only: [:show, :index, :create]
   end
 
   resources :items, only: [:show, :index, :create]
