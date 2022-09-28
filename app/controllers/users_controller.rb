401196081c6df3b8fdc3e7ecc6b494573fd04c55
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-09-28 14:27:52 +0300

    Done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,12 @@ class UsersController < ApplicationController
     user = User.find_by(id: params[:id])
     render json: user, include: :items
   end
+  
+  
+  private
+
+  def render_not_found_response
+    render json: { error: "User not found" }, status: :not_found
+  end
 
 end
